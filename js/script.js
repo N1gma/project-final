37465178896e0e918ca9d9a539b14abb33329e88
Alexander Chernobai <chornobai@mail.ru>
2016-08-06 14:49:35 +0300

    111

---
+++ js/script.js
@@ -0,0 +1,153 @@
+window.onload = function () {
+    //init
+
+    if(window.innerWidth < 1180 && window.innerWidth > 480 ){
+        tablet();
+    }
+    if(window.innerWidth <=480 ){
+        mobile();
+    }
+
+    var currentMargin = 0;
+    moveTo(currentMargin);
+    var timer = setInterval(moveRegular, 5000);
+
+    addEventListener('promo-2-container-1');
+    addEventListener('side-promo-banner');
+    
+    //promo
+
+    function moveRegular() {
+        if (currentMargin != '200') {
+            currentMargin += 100;
+        } else {
+            currentMargin = 0;
+        }
+        moveTo(currentMargin);
+    }
+
+    document.getElementsByClassName('promo-navigation')[0].addEventListener('click', function (e) {
+        clearInterval(timer);
+        currentMargin = +e.target.getAttribute('id');
+        moveTo(currentMargin);
+        timer = setInterval(moveRegular, 5000);
+    });
+
+
+    //search
+    document.getElementById('search-ico').addEventListener('click', function (e) {
+        document.getElementById('search-input').style.display = 'inline-block';
+        document.getElementById('search-input').focus();
+    });
+
+    document.getElementById('search-input').addEventListener('blur', function (e) {
+        var width = window.innerWidth;
+        if (width < 1080 && width > 480) {
+            document.getElementById('search-input').style.display = 'none';
+        }
+    });
+
+    //tablet menu
+    document.getElementsByClassName('tablet-menu')[0].addEventListener('click', function () {
+        if(document.getElementsByClassName('menu')[0].classList.contains('menu-visible')){
+            document.getElementsByClassName('menu')[0].classList.remove('menu-visible');
+            document.getElementById('switch').setAttribute('src','img/promo/mobile/menu.png');
+            document.getElementsByClassName('tablet-menu')[0].classList.add('button-element');
+        }else{
+            document.getElementsByClassName('menu')[0].classList.add('menu-visible');
+            document.getElementById('switch').setAttribute('src','img/promo/mobile/ico_close.png');
+            document.getElementsByClassName('tablet-menu')[0].classList.remove('button-element');
+        }
+    })
+};
+
+function moveTo(position) {
+    var active = document.getElementsByClassName('active')[0];
+    active.setAttribute('src', 'img/icons/promo_link_nonactive.png');
+    active.classList.remove('active');
+    document.getElementById("promoMain").style.marginLeft = -position + "%";
+    document.getElementById(position).setAttribute('src', 'img/icons/promo_link_active.png');
+    document.getElementById(position).classList.add('active');
+}
+
+function addEventListener(source) {
+    var promoSrcArray = [
+        ["img/promo/promo-img-2-compose/_2-1.png", "img/promo/promo-img-2-compose/_2-2.png"],
+        ["img/promo/promo-img-2-compose/_5-1.png", "img/promo/promo-img-2-compose/_5-2.png"],
+        ["img/promo/promo-img-2-compose/_6-1.png", "img/promo/promo-img-2-compose/_6-2.png"],
+        ['img/promo/tablet/banner_right_1.png','img/promo/tablet/banner_right_2.png'],
+        ['img/promo/mobile/banner_1.png','img/promo/mobile/banner_2.png']
+    ];
+
+    document.getElementsByClassName(source)[0].addEventListener('mouseover', function (e) {
+        var target = e.target.getAttribute('src');
+   
+        for (var i = 0; i < promoSrcArray.length; i++) {
+            for (var j = 0; j < promoSrcArray[i].length; j++) {
+                if (promoSrcArray[i][j] == target && promoSrcArray[i][j + 1]) {
+                    e.target.setAttribute('src', promoSrcArray[i][j + 1]);
+                }
+            }
+        }
+
+    });
+    document.getElementsByClassName(source)[0].addEventListener('mouseout', function (e) {
+        var target = e.target.getAttribute('src');
+        for (var i = 0; i < promoSrcArray.length; i++) {
+            for (var j = 0; j < promoSrcArray[i].length; j++) {
+                if (promoSrcArray[i][j] == target && promoSrcArray[i][j - 1]) {
+                    e.target.setAttribute('src', promoSrcArray[i][j - 1]);
+                }
+            }
+        }
+    });
+}
+
+window.onresize = function () {
+    if(window.innerWidth > 1180 ){
+        desktop();
+    }
+    if(window.innerWidth <= 1180 && window.innerWidth > 480 ){
+        tablet();
+    }
+    if(window.innerWidth <= 480 ){
+        mobile();
+    }
+};
+
+
+function desktop() {
+    var srcArray = ['img/promo/promo_img_1.jpg', 'img/promo/promo_img_3.jpg' ,
+        'img/promo/banner_left.jpg',"img/promo/banner_right.jpg"];
+    
+    document.getElementsByClassName('main-logo')[0].innerHTML = "Template";
+    document.getElementsByClassName('promo-1')[0].setAttribute('src', srcArray[0]);
+    document.getElementsByClassName('promo-3')[0].setAttribute('src', srcArray[1]);
+    document.getElementById('side-1').setAttribute('src', srcArray[2]);
+    document.getElementById('side-2').setAttribute('src', srcArray[3]);
+}
+
+function tablet() {
+    var srcArray = ['img/promo/tablet/_1.png','img/promo/tablet/_2.png', 'img/promo/tablet/_3.png' ,
+        'img/promo/tablet/banner_left.png',"img/promo/tablet/banner_right_1.png"];
+
+    document.getElementsByClassName('main-logo')[0].innerHTML = "Template";
+    document.getElementsByClassName('promo-1')[0].setAttribute('src', srcArray[0]);
+    document.getElementsByClassName('promo-2-tablet')[0].setAttribute('src', srcArray[1]);
+    document.getElementsByClassName('promo-3')[0].setAttribute('src', srcArray[2]);
+    document.getElementById('side-1').setAttribute('src', srcArray[3]);
+    document.getElementById('side-2').setAttribute('src', srcArray[4]);
+}
+
+function mobile() {
+    var srcArray = ['img/promo/mobile/_1.png','img/promo/mobile/_2.png', 'img/promo/mobile/_3.png' ,
+        'img/promo/mobile/banner_1.png'];
+
+    document.getElementsByClassName('main-logo')[0].innerHTML = "TL";
+    document.getElementsByClassName('promo-1')[0].setAttribute('src', srcArray[0]);
+    document.getElementsByClassName('promo-2-tablet')[0].setAttribute('src', srcArray[1]);
+    document.getElementsByClassName('promo-3')[0].setAttribute('src', srcArray[2]);
+    document.getElementById('side-1').setAttribute('src', srcArray[3]);
+
+   
+}
